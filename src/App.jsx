ebe0fce1a1a423ae0163a99e47cba7e69b411bc7
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2024-09-11 00:19:00 +0800

    Refactor: Add key prop to FormSplitBill component

--- src/App.jsx
+++ src/App.jsx
@@ -78,6 +78,7 @@ const App = () => {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
